834a0c72c2c45899496d753e1046ae2815f91d8b
Staszek Zajaczkowski <staszek.zajaczkowski@gmail.com>
2023-04-19 22:17:51 +0200

    created ContextProvider

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -4,7 +4,12 @@
 import * as React from 'react'
 
 // 🐨 create your CountContext here with React.createContext
-
+const CounterContext = React.createContext()
+const CountProvider = props => {
+  const [count, setCount] = React.useState(0)
+  const value = [count, setCount]
+  return <CounterContext.Provider value={value} {...props} />
+}
 // 🐨 create a CountProvider component here that does this:
 //   🐨 get the count state and setCount updater with React.useState
 //   🐨 create a `value` array with count and setCount
@@ -13,13 +18,15 @@ import * as React from 'react'
 
 function CountDisplay() {
   // 🐨 get the count from useContext with the CountContext
-  const count = 0
+  const [count] = React.useContext(CounterContext)
   return <div>{`The current count is ${count}`}</div>
 }
 
 function Counter() {
   // 🐨 get the setCount from useContext with the CountContext
-  const setCount = () => {}
+  const [, setCount] = React.useContext(CounterContext)
+
+  // const setCount = () => {}
   const increment = () => setCount(c => c + 1)
   return <button onClick={increment}>Increment count</button>
 }
@@ -31,8 +38,10 @@ function App() {
         🐨 wrap these two components in the CountProvider so they can access
         the CountContext value
       */}
-      <CountDisplay />
-      <Counter />
+      <CountProvider>
+        <CountDisplay />
+        <Counter />
+      </CountProvider>
     </div>
   )
 }
